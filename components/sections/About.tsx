7e95dabc8d4e78eb04cd5fcca2c866472a9e5ee7
Glenn Hensley III <glenniii.dev@gmail.com>
2025-09-26 14:54:35 -0400

    Resolved Error

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -5,7 +5,7 @@ export default function About() {
     <section className="bg-(--blue) rounded-3xl p-8 shadow-md transition-transform duration-300 hover:-translate-y-1">
         <h2 className="text-2xl font-semibold border-b-2 border-(--light-blue) pb-2 mb-4">About Me</h2>
         <div className="about-grid">
-          <p className="text-white leading-relaxed">Hey, I'm Glenn, a front-end developer who’s passionate about crafting clean, engaging websites. I love creating and transforming ideas through code.</p>
+          <p className="text-white leading-relaxed">Hey, I&apos;m Glenn, a web developer who&apos;s passionate about crafting clean, engaging websites. I love creating and transforming ideas through code.</p>
           <Image src="/images/glenniii.png" alt="Profile Picture" width={1000} height={1000} className="w-24 h-24 lg:w-32 lg:h-32 rounded-2xl border-2 border-(--light-blue) shadow-md transition-transform duration-300 hover:scale-105" />
         </div>
     </section>
